ad18830dccf66a64decf9fda58c232c15a8496cd
rocco barbini <roccobarbi@gmail.com>
2021-09-16 21:46:34 +0200

    content.js and calc.js linted and fixed

--- app/js/content.js
+++ app/js/content.js
@@ -1,106 +1,106 @@
 module.exports = (function() {
 
-    var fs = require('fs');
+    const fs = require('fs');
 
     // Edited original leaflet object
-    var augmentedLeafletDrawLocal = {
+    const augmentedLeafletDrawLocal = {
         draw: {
-    		toolbar: {
-    			actions: {
-    				title: 'Cancel',
-    				text: 'Cancel'
-    			},
-    			finish: {
-    				title: 'Finish',
-    				text: 'Finish'
-    			},
-    			undo: {
-    				title: 'Delete last point',
-    				text: 'Delete last point'
-    			},
-    			buttons: {
-    				polyline: 'Map a flight',
-    				polygon: 'Draw a polygon',
-    				rectangle: 'Draw a rectangle',
-    				circle: 'Draw a circle',
-    				marker: 'Mark a target'
-    			}
-    		},
-    		handlers: {
-    			circle: {
-    				tooltip: {
-    					start: 'Click and drag to draw circle'
-    				},
-    				radius: 'Radius'
-    			},
-    			marker: {
-    				tooltip: {
-    					start: 'Click to mark a target'
-    				}
-    			},
-    			polygon: {
-    				tooltip: {
-    					start: 'Click to start drawing shape',
-    					cont: 'Click to continue drawing shape',
-    					end: 'Click first point to close this shape'
-    				}
-    			},
-    			polyline: {
-    				error: '<strong>Error:</strong> shape edges cannot cross!',
-    				tooltip: {
-    					start: 'Click to start a flight plan',
-    					cont: 'Click to continue the flight plan',
-    					end: 'Click last point to finish flight plan'
-    				}
-    			},
-    			rectangle: {
-    				tooltip: {
-    					start: 'Click and drag to draw rectangle'
-    				}
-    			},
-    			simpleshape: {
-    				tooltip: {
-    					end: 'Release mouse to finish drawing'
-    				}
-    			}
-    		}
-    	},
-    	edit: {
-    		toolbar: {
-    			actions: {
-    				save: {
-    					title: 'Save changes',
-    					text: 'Save'
-    				},
-    				cancel: {
-    					title: 'Cancel editing, discard all changes',
-    					text: 'Cancel'
-    				}
-    			},
-    			buttons: {
-    				edit: 'Edit map',
-    				editDisabled: 'Nothing to edit',
-    				remove: 'Delete items from the map',
-    				removeDisabled: 'Nothing to delete'
-    			}
-    		},
-    		handlers: {
-    			edit: {
-    				tooltip: {
-    					text: 'Drag items to edit the map',
-    					subtext: null
-    				}
-    			},
-    			remove: {
-    				tooltip: {
-    					text: 'Click to delete items from the map'
-    				}
-    			}
-    		}
-    	}
+            toolbar: {
+                actions: {
+                    title: 'Cancel',
+                    text: 'Cancel'
+                },
+                finish: {
+                    title: 'Finish',
+                    text: 'Finish'
+                },
+                undo: {
+                    title: 'Delete last point',
+                    text: 'Delete last point'
+                },
+                buttons: {
+                    polyline: 'Map a flight',
+                    polygon: 'Draw a polygon',
+                    rectangle: 'Draw a rectangle',
+                    circle: 'Draw a circle',
+                    marker: 'Mark a target'
+                }
+            },
+            handlers: {
+                circle: {
+                    tooltip: {
+                        start: 'Click and drag to draw circle'
+                    },
+                    radius: 'Radius'
+                },
+                marker: {
+                    tooltip: {
+                        start: 'Click to mark a target'
+                    }
+                },
+                polygon: {
+                    tooltip: {
+                        start: 'Click to start drawing shape',
+                        cont: 'Click to continue drawing shape',
+                        end: 'Click first point to close this shape'
+                    }
+                },
+                polyline: {
+                    error: '<strong>Error:</strong> shape edges cannot cross!',
+                    tooltip: {
+                        start: 'Click to start a flight plan',
+                        cont: 'Click to continue the flight plan',
+                        end: 'Click last point to finish flight plan'
+                    }
+                },
+                rectangle: {
+                    tooltip: {
+                        start: 'Click and drag to draw rectangle'
+                    }
+                },
+                simpleshape: {
+                    tooltip: {
+                        end: 'Release mouse to finish drawing'
+                    }
+                }
+            }
+        },
+        edit: {
+            toolbar: {
+                actions: {
+                    save: {
+                        title: 'Save changes',
+                        text: 'Save'
+                    },
+                    cancel: {
+                        title: 'Cancel editing, discard all changes',
+                        text: 'Cancel'
+                    }
+                },
+                buttons: {
+                    edit: 'Edit map',
+                    editDisabled: 'Nothing to edit',
+                    remove: 'Delete items from the map',
+                    removeDisabled: 'Nothing to delete'
+                }
+            },
+            handlers: {
+                edit: {
+                    tooltip: {
+                        text: 'Drag items to edit the map',
+                        subtext: null
+                    }
+                },
+                remove: {
+                    tooltip: {
+                        text: 'Click to delete items from the map'
+                    }
+                }
+            }
+        }
     };
 
-    var mapConfigs = {
+    const mapConfigs = {
         stalingrad: {
             fullName: 'Stalingrad',
             name: 'stalingrad',
@@ -229,15 +229,15 @@ module.exports = (function() {
         }
     };
 
-    var defaults = {
-      flightName: 'New Flight',
-      flightSpeed: 300,
-      pointType: 'marker',
-      pointColor: 'black',
-      pointName: 'New Marker'
+    const defaults = {
+        flightName: 'New Flight',
+        flightSpeed: 300,
+        pointType: 'marker',
+        pointColor: 'black',
+        pointName: 'New Marker'
     };
 
-    var validatinatorConfig = {
+    const validatinatorConfig = {
         'grid-jump-form': {
             'grid-input': 'digitsLength:4'
         },
